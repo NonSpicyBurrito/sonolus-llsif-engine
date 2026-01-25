3b650d479acf77847502e093f2ff4630c5df395a
Burrito <burritoyangster@gmail.com>
2026-01-25 10:33:34 +0800

    Support Sonolus 1.1.0 (#33)

--- watch/src/engine/watchData/archetypes/notes/Note.ts
+++ watch/src/engine/watchData/archetypes/notes/Note.ts
@@ -41,7 +41,7 @@ export abstract class Note extends Archetype {
     globalPreprocess() {
         this.bucket.set(toBucketWindows(this.windows))
 
-        this.life.miss = -40
+        this.archetypeLife.miss = -40
     }
 
     preprocess() {
